5b7529a71da0472c08787653b5bc2ca283a429b5
Rohit Mishra <rohit@allotrop1.(none)>
2012-01-23 15:40:21 +0530

    Initial commit. Has placeholder FB SSO code.

---
+++ src/me/rohitmishra/groupbanker/GroupBankerActivity.java
@@ -0,0 +1,41 @@
+package me.rohitmishra.groupbanker;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Intent;
+import com.facebook.android.*;
+import com.facebook.android.Facebook.*;
+
+public class GroupBankerActivity extends Activity {
+	
+	Facebook facebook = new Facebook("228939630494756");
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        facebook.authorize(this, new DialogListener() {
+            @Override
+            public void onComplete(Bundle values) {}
+
+            @Override
+            public void onFacebookError(FacebookError error) {}
+
+            @Override
+            public void onError(DialogError e) {}
+
+            @Override
+            public void onCancel() {}
+        });
+        
+    }
+    
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        facebook.authorizeCallback(requestCode, resultCode, data);
+    }
+}
