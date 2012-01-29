11237a1b98b8a9bf66dff31c40b863098072f3f6
Rohit Mishra <rohit@rohitmishra.me>
2012-01-29 19:24:40 +0530

    Change code as per Hackbook
    Hackbook seems to be pretty well written and I have adapted our
    GroupBankerActivity to resemble Hackbook.
    
    This current version will not use WelcomeActivity. Most of the new files and
    resources have been taken from the Hackbook example.

--- gen/me/rohitmishra/groupbanker/R.java
+++ gen/me/rohitmishra/groupbanker/R.java
@@ -10,13 +10,31 @@ package me.rohitmishra.groupbanker;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int black=0x7f040001;
+        public static final int blue=0x7f040005;
+        public static final int green=0x7f040002;
+        public static final int grey=0x7f040006;
+        public static final int lite_blue=0x7f040004;
+        public static final int orange=0x7f040003;
+        public static final int white=0x7f040000;
+    }
     public static final class drawable {
         public static final int close=0x7f020000;
         public static final int facebook_icon=0x7f020001;
         public static final int ic_launcher=0x7f020002;
+        public static final int login=0x7f020003;
+        public static final int login_button=0x7f020004;
+        public static final int login_down=0x7f020005;
+        public static final int logout=0x7f020006;
+        public static final int logout_button=0x7f020007;
+        public static final int logout_down=0x7f020008;
     }
     public static final class id {
-        public static final int FBconnect=0x7f050000;
+        public static final int FBconnect=0x7f060003;
+        public static final int login=0x7f060002;
+        public static final int txt=0x7f060001;
+        public static final int user_pic=0x7f060000;
     }
     public static final class layout {
         public static final int fbdone=0x7f030000;
@@ -24,9 +42,9 @@ public final class R {
         public static final int welcome=0x7f030002;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int fbdone=0x7f040003;
-        public static final int fblogin=0x7f040002;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f050001;
+        public static final int fbdone=0x7f050003;
+        public static final int fblogin=0x7f050002;
+        public static final int hello=0x7f050000;
     }
 }
--- src/me/rohitmishra/groupbanker/GroupBankerActivity.java
+++ src/me/rohitmishra/groupbanker/GroupBankerActivity.java
@@ -1,18 +1,51 @@
 package me.rohitmishra.groupbanker;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.content.Intent;
 import android.content.SharedPreferences ;
 import com.facebook.android.*;
+import com.facebook.android.R;
 import com.facebook.android.Facebook.*;
+import me.rohitmishra.groupbanker.GroupBankerActivity.FbAPIsAuthListener;
+import me.rohitmishra.groupbanker.GroupBankerActivity.FbAPIsLogoutListener;
+import me.rohitmishra.groupbanker.GroupBankerActivity.UserRequestListener;
+import me.rohitmishra.groupbanker.SessionEvents.AuthListener;
+import me.rohitmishra.groupbanker.SessionEvents.LogoutListener;
 import com.facebook.*;
+import me.rohitmishra.groupbanker.LoginButton ;
+import me.rohitmishra.groupbanker.Utility ;
 
 public class GroupBankerActivity extends Activity {
 	
 	private static final String TAG = "GroupBankerActivity";
+	
+	/* Hackbook wants us to initialize FB class via the Utility class 
+	
 	Facebook facebook = new Facebook("228939630494756");
+	*/
+	
+	// Copied from Hackbook for Android 
+	
+	public static final String APP_ID = "228939630494756";
+	private LoginButton mLoginButton ;
+	private TextView mText;
+    private ImageView mUserPic;
+	private Handler mHandler;
+    ProgressDialog dialog;
+    String[] permissions = { "offline_access", "publish_stream", "user_photos", "publish_checkins",
+    "photo_upload" };
+    final static int AUTHORIZE_ACTIVITY_RESULT_CODE = 0;
+    
 	String FILENAME = "AndroidSSO_data";
     private SharedPreferences mPrefs;
     
@@ -22,7 +55,25 @@ public class GroupBankerActivity extends Activity {
     	super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
+        mHandler = new Handler() ;
         
+        mText = (TextView) GroupBankerActivity.this.findViewById(R.id.txt);
+        mUserPic = (ImageView) GroupBankerActivity.this.findViewById(R.id.user_pic);
+        
+     // Create the Facebook Object using the app id.
+        Utility.mFacebook = new Facebook(APP_ID);
+        // Instantiate the asynrunner object for asynchronous api calls.
+        Utility.mAsyncRunner = new AsyncFacebookRunner(Utility.mFacebook);
+
+        mLoginButton = (LoginButton) findViewById(R.id.login);
+        
+     // restore session if one exists
+        SessionStore.restore(Utility.mFacebook, this);
+        SessionEvents.addAuthListener(new FbAPIsAuthListener());
+        SessionEvents.addLogoutListener(new FbAPIsLogoutListener());
+
+        
+       /* This whole block of code is being commented in favor of the Hackbook code
         //  Get existing access_token if any
          
         mPrefs = getPreferences(MODE_PRIVATE);
@@ -31,6 +82,13 @@ public class GroupBankerActivity extends Activity {
         if(access_token != null) {
             facebook.setAccessToken(access_token);
         }
+        
+        mLoginButton.init(this, AUTHORIZE_ACTIVITY_RESULT_CODE, Utility.mFacebook, permissions);
+
+        if (Utility.mFacebook.isSessionValid()) {
+            requestUserData();
+        }
+        
         
         if(expires != 0) {
             facebook.setAccessExpires(expires);
@@ -46,5 +104,130 @@ public class GroupBankerActivity extends Activity {
         	Intent i = new Intent(this, WelcomeActivity.class);
         	startActivity(i);
          }
+         
+         */
+        
+    }		// End of onCreate 
+    
+    @Override
+    public void onResume() {
+        super.onResume();
+        if(Utility.mFacebook != null) {
+            if (!Utility.mFacebook.isSessionValid()) {
+                mText.setText("You are logged out! ");
+                mUserPic.setImageBitmap(null);
+            } else {
+                Utility.mFacebook.extendAccessTokenIfNeeded(this, null);
+            }
+        }
+    }
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+        /*
+         * if this is the activity result from authorization flow, do a call
+         * back to authorizeCallback Source Tag: login_tag
+         */
+            case AUTHORIZE_ACTIVITY_RESULT_CODE: {
+                Utility.mFacebook.authorizeCallback(requestCode, resultCode, data);
+                break;
+            }
+        }
+    }
+   
+    public class FQLRequestListener extends BaseRequestListener {
+
+        @Override
+        public void onComplete(final String response, final Object state) {
+            mHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(getApplicationContext(), "Response: " + response,
+                            Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+
+        public void onFacebookError(FacebookError error) {
+            Toast.makeText(getApplicationContext(), "Facebook Error: " + error.getMessage(),
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+    /*
+     * Callback for fetching current user's name, picture, uid.
+     */
+    public class UserRequestListener extends BaseRequestListener {
+
+        @Override
+        public void onComplete(final String response, final Object state) {
+            JSONObject jsonObject;
+            try {
+                jsonObject = new JSONObject(response);
+
+                final String picURL = jsonObject.getString("picture");
+                final String name = jsonObject.getString("name");
+                Utility.userUID = jsonObject.getString("id");
+
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        mText.setText("Welcome " + name + "!");
+                        mUserPic.setImageBitmap(Utility.getBitmap(picURL));
+                    }
+                });
+
+            } catch (JSONException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    /*
+     * The Callback for notifying the application when authorization succeeds or
+     * fails.
+     */
+
+    public class FbAPIsAuthListener implements AuthListener {
+
+        @Override
+        public void onAuthSucceed() {
+            requestUserData();
+        }
+
+        @Override
+        public void onAuthFail(String error) {
+            mText.setText("Login Failed: " + error);
+        }
+    }
+
+    /*
+     * The Callback for notifying the application when log out starts and
+     * finishes.
+     */
+    public class FbAPIsLogoutListener implements LogoutListener {
+        @Override
+        public void onLogoutBegin() {
+            mText.setText("Logging out...");
+        }
+
+        @Override
+        public void onLogoutFinish() {
+            mText.setText("You have logged out! ");
+            mUserPic.setImageBitmap(null);
+        }
+    }
+
+    /*
+     * Request user name, and picture to show on the main screen.
+     */
+    public void requestUserData() {
+        mText.setText("Fetching user name, profile pic...");
+        Bundle params = new Bundle();
+        params.putString("fields", "name, picture");
+        Utility.mAsyncRunner.request("me", params, new UserRequestListener());
     }
 }
