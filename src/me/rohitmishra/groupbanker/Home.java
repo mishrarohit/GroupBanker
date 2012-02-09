ce84c0eb92304ec72d3e93c644dcdc6f9aefd3df
Samridhi Shree <sami@samridhishree.com>
2012-02-10 01:38:25 +0530

    added 3 tabs in home activity

--- src/me/rohitmishra/groupbanker/Home.java
+++ src/me/rohitmishra/groupbanker/Home.java
@@ -3,6 +3,7 @@ package me.rohitmishra.groupbanker;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TabHost;
 
 public class Home extends Activity	{
 	
@@ -15,6 +16,25 @@ public class Home extends Activity	{
 		super.onCreate(savedInstanceState) ;
 		
 		setContentView(R.layout.home) ;
+		
+		TabHost tabs=(TabHost)findViewById(R.id.tabhost);
+		
+		tabs.setup();
+		
+		TabHost.TabSpec spec=tabs.newTabSpec("tag1");
+		spec.setContent(R.id.tab1);
+		spec.setIndicator("Overview");
+		tabs.addTab(spec);
+		
+		spec=tabs.newTabSpec("tag2");
+		spec.setContent(R.id.tab2);
+		spec.setIndicator("New Expense");
+		tabs.addTab(spec);
+		
+		spec = tabs.newTabSpec("tag3");
+		spec.setContent(R.id.tab3);
+		spec.setIndicator("History");
+		tabs.addTab(spec);
 		
 	}
 	
