8239b2ca74b4dc44c32c3e85ce660dd089960d23
Rohit Mishra <rohit@rohitmishra.me>
2012-02-17 22:10:01 +0530

    Added couple of boxes to add transaction
    Cleared values added by hackbook from strings.xml

--- src/me/rohitmishra/groupbanker/AddTransactionActivity.java
+++ src/me/rohitmishra/groupbanker/AddTransactionActivity.java
@@ -7,9 +7,6 @@ import android.widget.TextView;
 public class AddTransactionActivity extends Activity{
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        TextView textview = new TextView(this);
-        textview.setText("This is the Add Transaction tab");
-        setContentView(textview);
+        setContentView(R.layout.addtransaction);
     }
 }
