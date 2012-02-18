ae66498998811f8f7a1b2edaf00f7badfa48a092
Samridhi Shree <sami@samridhishree.com>
2012-02-18 15:20:55 +0530

    new activity selectFriends for list of friends

--- src/me/rohitmishra/groupbanker/AddTransactionActivity.java
+++ src/me/rohitmishra/groupbanker/AddTransactionActivity.java
@@ -1,12 +1,30 @@
 package me.rohitmishra.groupbanker;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.Button;
+import android.view.View;
 
-public class AddTransactionActivity extends Activity{
+public class AddTransactionActivity extends Activity implements View.OnClickListener{
+	
+	Button btn;
+	
+	@Override
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.addtransaction);
-    }
+        
+        btn = (Button)findViewById(R.id.addFriends);
+        btn.setOnClickListener(this);
+        
+      }
+	
+	@Override
+	public void onClick(View view) {
+		
+		Intent selectIntent = new Intent(getApplicationContext(), selectFriends.class);
+        startActivityForResult(selectIntent, 0);
+	}
+
 }
