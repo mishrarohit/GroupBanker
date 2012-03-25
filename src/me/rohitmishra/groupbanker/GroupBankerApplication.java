130a36433967e635ec7c5d73c94df6ceda167692
Rohit Mishra <rohit@rohitmishra.me>
2012-03-25 23:18:15 +0530

    Fixed a problem with retrieving username.
    We were retrieving username and FB ID in onCreate of
    GroupBankerApplication which is an incorrect method. Explained in
    comments.

--- src/me/rohitmishra/groupbanker/GroupBankerApplication.java
+++ src/me/rohitmishra/groupbanker/GroupBankerApplication.java
@@ -16,26 +16,27 @@ public class GroupBankerApplication extends Application {
 	public void onCreate()	{
 		friendsDbHelper = new FriendsDbAdapter(this);
 		friendsDbHelper.open() ;
-	
-		try {
+		
 		mPrefs = getSharedPreferences(Constants.preferences, 0);
-		this.userFBID = mPrefs.getString("userfbid", null);
-		this.userName = mPrefs.getString("username", null);
-		} catch (Exception e)	{
-			Log.e(TAG, "Problem in mPrefs = " + e.toString());
-		}
 	}
 
 	public FriendsDbAdapter getFriendsDbAdapter()	{
 		return this.friendsDbHelper ;
 	}
 
+	/* We were earlier retrieving UserFBID and UserName from SharedPreferences in onCreate. 
+	 * The bug that was coming was because GroupBankerApplication may (or always will) get initialized
+	 * before other classes. It's onCreate will then assign these two variables null value.
+	 */
+	
 	public String getUserFBID() {
+		this.userFBID = mPrefs.getString("userfbid", null);
 		return this.userFBID;
 	}
 
 	public String getUserName() {
-		Log.v(TAG, "Username Getting returned:" + userName);
+		this.userName = mPrefs.getString("username", null);
+		Log.v(TAG, "Username Getting returned:" + this.userName);
 		return this.userName;
 	}
 	
